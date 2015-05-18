358976e5a4964b64d129af11b18b49da0d082a41
necrotv <larala12>
2015-05-18 16:36:50 +0200

    x

---
+++ script.tvguidemicro/logoDownloader.py
@@ -0,0 +1,29 @@
+import xbmc
+import xbmcaddon
+import download
+import extract
+import base64
+
+
+ADDON  = xbmcaddon.Addon(id = 'script.tvguidemicro')
+datapath = xbmc.translatePath(ADDON.getAddonInfo('profile'))
+extras   = os.path.join(datapath, 'extras')
+logos    = os.path.join(extras, 'logos')
+nologos  = os.path.join(logos, 'None')
+dest     = os.path.join(extras, 'logos.zip')
+url      = base64.b64decode('aHR0cDovL3R2bmVjcm8uaG9ub3IuZXMvX2d1aWRlL2xvZ29zLnppcA==')
+
+
+try:
+    os.makedirs(logos)
+    os.makedirs(nologos)
+except:
+    pass
+
+download.download(url, dest)
+extract.all(dest, extras)
+
+try:
+    os.remove(dest)
+except:
+    pass
